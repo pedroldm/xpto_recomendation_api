c84cf2d1e26e77547a7d9465eb4826668946dcca
Pedro Lucas Damasceno <pedroldmf@gmail.com>
2024-09-01 11:53:46 -0300

    Adding recommendation bias variables to .env

--- src/config.py
+++ src/config.py
@@ -5,6 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_PATH = os.getenv("DB_PATH")
+SALES_BIAS = os.getenv("SALES_BIAS")
+PRICE_BIAS = os.getenv("PRICE_BIAS")
+STORE_SALES_BIAS = os.getenv("STORE_SALES_BIAS")
 
 def get_database_path() -> str:
     return DB_PATH
+
+def get_sales_bias() -> float:
+    return SALES_BIAS
+
+def get_price_bias() -> float:
+    return PRICE_BIAS
+
+def get_store_sales_bias() -> float:
+    return STORE_SALES_BIAS
